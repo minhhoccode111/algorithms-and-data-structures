d3492e500ce030a1f0e7733fb0d13a379f396ead
minhhoccode111 <minhhoccode111@gmail.com>
2023-08-23 21:29:53 +0700

    1.two sum & 167.two sum II array sorted

---
+++ platform-solutions/leetcode/1.two-sum.js
@@ -0,0 +1,39 @@
+console.log('hello world');
+
+// EASY
+
+// simple solution with 2 nested loops
+// Big O(n^2) - time complexity
+var twoSum = function (nums, target) {
+  for (let i = 0; i <= nums.length - 2; i++) {
+    for (let j = i + 1; j <= nums.length - 1; j++) {
+      if (nums[i] + nums[j] === target) return [i, j];
+    }
+  }
+};
+// => we loop through each item, every item we loop again but current item to check if it sum up to target
+
+// better solution with 2 separated loops
+// Big O(n) - time complexity (or Big O(2n))
+var twoSum = (nums, target) => {
+  const hash = {};
+
+  for (let i = 0; i < nums.length; i++) {
+    hash[nums[i]] = i;
+  }
+
+  for (let j = 0; j < nums.length; j++) {
+    /**
+        hash = {
+            '3':0,
+            '2':1,
+            '4':2,
+        }
+       */
+    const diff = target - nums[j];
+    if (hash[diff] && hash[diff] !== j) {
+      return [hash[diff], j];
+    }
+  }
+};
+// => we store each item in array to a hash table with the item in array will be key in hash table and index of that item in the array will be the value of that key in hash table, and we loop through array again and check if the diff between target and current item in array actually exist in the hash table and it's not the same index as current item in the array, if it's existed then return
