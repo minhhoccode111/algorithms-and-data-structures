e8121e8d185515cfce49910dd3ea864261c724eb
minhhoccode111 <minhhoccode111@gmail.com>
2023-08-25 00:49:14 +0700

    adding recursion solution to roman to integer

--- platform-solutions/leetcode/13.roman-to-integer.js
+++ platform-solutions/leetcode/13.roman-to-integer.js
@@ -1,41 +1,63 @@
 console.log('Hello, World!');
 
+// simple solution with loop
+// Big O(n) time complexity - Big O(n) space complexity
+const romanToInt = (s) => {
+  let result = 0,
+    current = 0,
+    next = 1,
+    l = s.length;
+  const DATA = { I: 1, V: 5, X: 10, L: 50, C: 100, D: 500, M: 1000, IV: 4, IX: 9, XL: 40, XC: 90, CD: 400, CM: 900 };
+  const ARR = ['I', 'V', 'X', 'L', 'C', 'D', 'M'];
+  while (current < l) {
+    const currentChar = s[current],
+      nextChar = s[next];
+    const isCombinationOfTwo = ARR.indexOf(currentChar) < ARR.indexOf(nextChar);
+    if (isCombinationOfTwo) {
+      result += DATA[currentChar + nextChar];
+      current += 2;
+      next += 2;
+      continue;
+    }
+    result += DATA[currentChar];
+    current++;
+    next++;
+  }
+  return result;
+};
+
 // simple solution with recursion
-const convert = (str) => {
-  const DATA = {
-    I: 1,
-    V: 5,
-    X: 10,
-    L: 50,
-    C: 100,
-    D: 500,
-    M: 1000,
-    IV: 4,
-    IX: 9,
-    XL: 40,
-    XC: 90,
-    CD: 400,
-    CM: 900,
-  };
-  return DATA[str];
+// make it a little bit faster and a little bit less space consumption :)
+// Big O(n) time complexity - Big O(n) space complexity
+
+const romanToIntRec = (
+  s,
+  result = 0,
+  current = 0,
+  next = 1,
+  l = s.length,
+  DATA = { I: 1, V: 5, X: 10, L: 50, C: 100, D: 500, M: 1000, IV: 4, IX: 9, XL: 40, XC: 90, CD: 400, CM: 900 },
+  ARR = ['I', 'V', 'X', 'L', 'C', 'D', 'M']
+) => {
+  if (current === l) return result;
+
+  if (ARR.indexOf(s[current]) < ARR.indexOf(s[next])) return romanToIntRec(s, result + DATA[s[current] + s[next]], current + 2, next + 2, l, DATA, ARR);
+
+  return romanToIntRec(s, result + DATA[s[current]], current + 1, next + 1, l, DATA, ARR);
 };
 
-const checkGreater = (char1, char2) => {
-  const arr = ['I', 'V', 'X', 'L', 'C', 'D', 'M'];
-  // if true then must be a combining of 2 Character
-  return arr.indexOf(char1) < arr.indexOf(char2);
-};
-
-var romanToInt = function (s) {
-  if (s.length === 0) {
-    return 0;
-  }
-
-  // if something like IV, IX, XL, XC, CD, CM
-  if (checkGreater(s[0], s[1])) {
-    return convert(s.slice(0, 2)) + romanToInt(s.slice(2));
-  } else {
-    // else just one at a time
-    return convert(s.slice(0, 1)) + romanToInt(s.slice(1));
-  }
-};
+// or even make it so short that no one could read or understand :)
+const romanToIntRecShort = (
+  s,
+  result = 0,
+  current = 0,
+  next = 1,
+  l = s.length,
+  DATA = { I: 1, V: 5, X: 10, L: 50, C: 100, D: 500, M: 1000, IV: 4, IX: 9, XL: 40, XC: 90, CD: 400, CM: 900 },
+  ARR = ['I', 'V', 'X', 'L', 'C', 'D', 'M']
+) =>
+  current === l
+    ? result
+    : ARR.indexOf(s[current]) < ARR.indexOf(s[next])
+    ? romanToIntRecShort(s, result + DATA[s[current] + s[next]], current + 2, next + 2, l, DATA, ARR)
+    : romanToIntRecShort(s, result + DATA[s[current]], current + 1, next + 1, l, DATA, ARR);
