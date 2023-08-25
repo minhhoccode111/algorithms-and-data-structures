59ed8a139a1e124ec9c82a00382867abcc92261b
minhhoccode111 <minhhoccode111@gmail.com>
2023-08-25 23:38:47 +0700

    9.palindrome number

--- platform-solutions/leetcode/merge-two-sorted-lists.js
+++ platform-solutions/leetcode/merge-two-sorted-lists.js
@@ -1,6 +1,1 @@
-let list1 = [1, 2, 4],
-  list2 = [1, 3, 4];
-var mergeTwoLists = function (list1, list2) {
-  return list1.concat(list2).sort((a, b) => (a > b ? 1 : -1));
-};
-console.log(mergeTwoLists(list1, list2));
+//
